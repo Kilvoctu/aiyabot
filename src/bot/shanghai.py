db8d0c247581c4332222909a4c1d23442b2ecd14
pepe10-gpu <pepe.dannyboy@gmail.com>
2022-09-16 00:43:05 -0700

    okay actually

--- src/bot/shanghai.py
+++ src/bot/shanghai.py
@@ -4,17 +4,17 @@ from discord.ext import commands
 from src.core.logging import get_logger
 
 class Shanghai(commands.Bot):
-    async def __init__(self, args):        
+    def __init__(self, args):
         intents = discord.Intents.default()
         intents.members = True
         super().__init__(command_prefix=args.prefix, intents=intents)
         self.args = args
         self.logger = get_logger(__name__)
-        await self.load_extension('src.bot.stablecog')
 
     async def on_ready(self):
         self.logger.info(f'Logged in as {self.user.name} ({self.user.id})')
         await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='you over the seven seas.'))
+        await self.load_extension('src.bot.stablecog')
     
     async def close(self):
         await self._bot.close()
