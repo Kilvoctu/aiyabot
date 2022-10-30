eae34b1da3de0d7f65e562a7c296c61d3cc2d058
Kilvoctu <kilvoctu@zoho.com>
2022-10-30 16:58:23 -0500

    fix issue with saving png info

--- core/stablecog.py
+++ core/stablecog.py
@@ -3,6 +3,7 @@ import base64
 import csv
 import discord
 import io
+import json
 import os
 import random
 import requests
@@ -235,6 +236,9 @@ class StableCog(commands.Cog, name='Stable Diffusion', description='Create image
             response_data = response.json()
             end_time = time.time()
 
+            #grab png info
+            load_r = json.loads(response_data['info'])
+            meta = load_r["infotexts"][0]
             #create safe/sanitized filename
             keep_chars = (' ', '.', '_')
             file_name = "".join(c for c in queue_object.prompt if c.isalnum() or c in keep_chars).rstrip()
@@ -243,7 +247,7 @@ class StableCog(commands.Cog, name='Stable Diffusion', description='Create image
                 image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[1])))
                 metadata = PngImagePlugin.PngInfo()
                 epoch_time = int(time.time())
-                metadata.add_text("parameters", str(response_data['info']))
+                metadata.add_text("parameters", meta)
                 image.save(f'{settings.global_var.dir}\{epoch_time}-{queue_object.seed}-{file_name[0:120]}.png', pnginfo=metadata)
                 print(f'Saved image: {settings.global_var.dir}\{epoch_time}-{queue_object.seed}-{file_name[0:120]}.png')
 
