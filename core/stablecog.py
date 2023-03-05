f7dcf5a1c3cf0ac2f40ac47720ea81f25e0d67aa
Kilvoctu <kilvoctu@zoho.com>
2023-03-04 19:33:40 -0600

    refactor discord post queueing
    also add code to edit Discord View to increment seed in batch (but why is it not working)

--- core/stablecog.py
+++ core/stablecog.py
@@ -10,7 +10,6 @@ from asyncio import AbstractEventLoop
 from PIL import Image, PngImagePlugin
 from discord import option
 from discord.ext import commands
-from threading import Thread
 from typing import Optional
 
 from core import queuehandler
@@ -350,6 +349,7 @@ class StableCog(commands.Cog, name='Stable Diffusion', description='Create image
 
     # the function to queue Discord posts
     def post(self, event_loop: AbstractEventLoop, post_queue_object: queuehandler.PostObject):
+        print(f'post_queue seed - {post_queue_object.view.input_tuple[10]}')
         event_loop.create_task(
             post_queue_object.ctx.channel.send(
                 content=post_queue_object.content,
@@ -466,28 +466,35 @@ class StableCog(commands.Cog, name='Stable Diffusion', description='Create image
 
                 settings.stats_count(1)
 
+                # set up discord message
+                content = ''
+                image_count = len(image_data)
+                noun_descriptor = "drawing" if image_count == 1 else f'{image_count} drawings'
+                draw_time = '{0:.3f}'.format(end_time - start_time)
+                message = f'my {noun_descriptor} of ``{queue_object.simple_prompt}`` took me ``{draw_time}`` seconds!'
+
+                batch_seed = list(queue_object.view.input_tuple)
+
+                if count == 1:
+                    content = f'<@{queue_object.ctx.author.id}>, {message}'
+                    batch_seed[10] -= 1
+
+                batch_seed[10] += 1
+                new_tuple = tuple(batch_seed)
+                queue_object.view.input_tuple = new_tuple
+                new_view = queue_object.view
+                print(f'count - {count}, new seed - {new_view.input_tuple[10]}')
+
                 # post to discord
-                def post_dream():
-                    content = ''
-                    image_count = len(image_data)
-                    noun_descriptor = "drawing" if image_count == 1 else f'{image_count} drawings'
-                    draw_time = '{0:.3f}'.format(end_time - start_time)
-                    message = f'my {noun_descriptor} of ``{queue_object.simple_prompt}`` took me ``{draw_time}`` seconds!'
+                with io.BytesIO() as buffer:
+                    image = Image.open(io.BytesIO(base64.b64decode(i)))
+                    image.save(buffer, 'PNG', pnginfo=metadata)
+                    buffer.seek(0)
 
-                    if count == 1:
-                        content = f'<@{queue_object.ctx.author.id}>, {message}'
-
-                    with io.BytesIO() as buffer:
-                        image = Image.open(io.BytesIO(base64.b64decode(i)))
-                        image.save(buffer, 'PNG', pnginfo=metadata)
-                        buffer.seek(0)
-
-                        file = discord.File(fp=buffer, filename=f'{queue_object.seed}-{count}.png')
-
-                        queuehandler.process_post(
-                            self, queuehandler.PostObject(
-                                self, queue_object.ctx, content=content, file=file, embed='', view=queue_object.view))
-                Thread(target=post_dream, daemon=True).start()
+                    file = discord.File(fp=buffer, filename=f'{queue_object.seed}-{count}.png')
+                    queuehandler.process_post(
+                        self, queuehandler.PostObject(
+                            self, queue_object.ctx, content=content, file=file, embed='', view=new_view))
 
         except KeyError:
             embed = discord.Embed(title='txt2img failed', description=f'An invalid parameter was found!',
