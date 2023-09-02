3650a9ae72b95a023789711b5dca9ba49d4a302f
Wizz_^ <lewizard@hotmail.fr>
2023-09-02 07:30:53 +0200

    Add files via upload
    Change model for /Generate, way better
    From Gustavosta to FredZhang7

--- core/setup_generate.py
+++ core/setup_generate.py
@@ -1,16 +1,15 @@
 import urllib.request
 import os
 
-model_folder = "core/MagicPrompt-SD"
+model_folder = "core/DistilGPT2-Stable-Diffusion-V2"
 os.makedirs(model_folder, exist_ok=True)
 
-base_url = "https://huggingface.co/Gustavosta/MagicPrompt-Stable-Diffusion/resolve/main/"
+base_url = "https://huggingface.co/FredZhang7/distilgpt2-stable-diffusion-v2/resolve/main/"
 
-files_to_download = ["config.json", "merges.txt", "pytorch_model.bin", "vocab.json"]
+files_to_download = ["config.json", "pytorch_model.bin", "tokenizer.json", "training_args.bin"]
 
 for file in files_to_download:
     filepath = os.path.join(model_folder, file)
     if not os.path.isfile(filepath):
         print(f"Missing file for /generate. Downloading {file}")
         urllib.request.urlretrieve(base_url + file, filepath)
-    
