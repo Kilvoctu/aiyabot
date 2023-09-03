029d603e3a19710124c0de63457905693f42328c
Wizz_^ <lewizard@hotmail.fr>
2023-09-03 02:32:22 +0200

    Add files via upload

--- core/generatecog.py
+++ core/generatecog.py
@@ -15,25 +15,50 @@ from core import settings
 
 class PromptButton(Button):
     def __init__(self, label, prompt_index, parent_view):
-        super().__init__(label=label, custom_id=f"prompt_{prompt_index}")
+        super().__init__(label=label, custom_id=f"prompt_{prompt_index}", emoji="🎨")
         self.parent_view = parent_view
 
     async def callback(self, interaction):
-        prompt_index = int(self.custom_id.split("_")[1])
-        prompt = self.parent_view.prompts[prompt_index]
-        await interaction.response.defer()
-        await self.parent_view.stable_cog.dream_handler(self.parent_view.ctx, prompt=prompt, called_from_button=True)
+        try:
+            prompt_index = int(self.custom_id.split("_")[1])
+            prompt = self.parent_view.prompts[prompt_index]
+            await interaction.response.defer()
+            await self.parent_view.stable_cog.dream_handler(self.parent_view.ctx, prompt=prompt, called_from_button=True)
+        except Exception as e:
+            print(f'The draw button broke: {str(e)}')
+            self.disabled = True
+            await interaction.response.edit_message(view=self.parent_view)
+            await interaction.followup.send("I may have been restarted. This button no longer works.", ephemeral=True)
+
+
+class DeleteButton(Button):
+    def __init__(self, parent_view):
+        super().__init__(label="Delete", custom_id="delete", emoji="❌")
+        self.parent_view = parent_view
+
+    async def callback(self, interaction):
+        try:
+            await self.parent_view.message.delete()
+            await interaction.response.defer()
+        except Exception as e:
+            print(f'The delete button broke: {str(e)}')
+            self.disabled = True
+            await interaction.response.edit_message(view=self.parent_view)
+            await interaction.followup.send("I may have been restarted. This button no longer works.", ephemeral=True)
 
 
 class GenerateView(View):
-    def __init__(self, prompts, stable_cog, ctx):
+    def __init__(self, prompts, stable_cog, ctx, message):
         super().__init__()
         self.stable_cog = stable_cog
         self.ctx = ctx
         self.prompts = prompts
+        self.message = message
         for i, prompt in enumerate(prompts):
             button = PromptButton(label=f"Draw n°{i+1}", prompt_index=i, parent_view=self)
             self.add_item(button)
+        delete_button = DeleteButton(parent_view=self)
+        self.add_item(delete_button)
 
     async def interaction_check(self, interaction):
         return True
@@ -45,7 +70,7 @@ class GenerateCog(commands.Cog):
         self.model_path = "core/DistilGPT2-Stable-Diffusion-V2/"
         tokenizer = AutoTokenizer.from_pretrained(self.model_path)
         model = AutoModelForCausalLM.from_pretrained(self.model_path)
-        self.pipe = pipeline('text-generation', model=model, tokenizer=tokenizer, max_length=100)
+        self.pipe = pipeline('text-generation', model=model, tokenizer=tokenizer, max_length=75)
 
     @commands.slash_command(name='generate', description='Generates a prompt from text', guild_only=True)
     @option(
@@ -54,14 +79,35 @@ class GenerateCog(commands.Cog):
         description='Your text to produce the prompt.',
         required=True,
     )
+    @option(
+        'Number of prompts',
+        str,
+        description='The number of prompts to produce. (1-5)',
+        required=False,
+    )
+    @option(
+        'Max length',
+        str,
+        description='The max length for the generated prompts. (15-150)',
+        required=False,
+    )
     async def generate_handler(self, ctx: discord.ApplicationContext, *,
                             prompt: Optional[str],
                             num_prompts: Optional[int]=1,
-                            max_length: Optional[int]=100):
+                            max_length: Optional[int]=75):
 
         # console log
         print(f"/Generate request for {num_prompts} prompt(s) of {max_length} tokens. Text: {prompt}")
         
+        # sanity check
+        if not 1 <= num_prompts <= 5:
+            await ctx.send("The number of prompts must be between 1 and 5.")
+            return
+            
+        if not 15 <= max_length <= 150:
+            await ctx.send("The maximum length must be between 15 and 150.")
+            return
+            
         # set up the queue
         if queuehandler.GlobalQueue.generate_thread.is_alive():
             queuehandler.GlobalQueue.generate_queue.append(queuehandler.GenerateObject(self, ctx, prompt, num_prompts, max_length))
@@ -69,7 +115,7 @@ class GenerateCog(commands.Cog):
             await queuehandler.process_generate(self, queuehandler.GenerateObject(self, ctx, prompt, num_prompts, max_length))
 
         await ctx.send_response(f"<@{ctx.author.id}>, {settings.messages()}\nQueue: ``{len(queuehandler.GlobalQueue.generate_queue)}`` - Your text: ``{prompt}``\nNumber of prompts: ``{num_prompts}`` - Max length: ``{max_length}``")
-
+        
     def post(self, event_loop: AbstractEventLoop, post_queue_object: queuehandler.PostObject):
         event_loop.create_task(
             post_queue_object.ctx.channel.send(
@@ -103,15 +149,18 @@ class GenerateCog(commands.Cog):
     
     async def send_with_view(self, prompts, ctx):
         stable_cog = self.bot.get_cog('Stable Diffusion')
-        view = GenerateView(prompts, stable_cog, ctx)
-
-        # Create an Embed object
+        
+        # create embed
         title = "What about this as Prompt?!" if len(prompts) == 1 else "What about these as Prompts?!"
         numbered_prompts = [f"**Prompt {i+1}:**\n{prompt}" for i, prompt in enumerate(prompts)]
         embed = discord.Embed(title=title, description="\n\n".join(numbered_prompts), color=0x00ff00)
 
         # post to discord
-        await ctx.send(content=f'<@{ctx.author.id}>', embed=embed, view=view)
+        message = await ctx.send(content=f'<@{ctx.author.id}>', embed=embed)
+
+        # create view
+        view = GenerateView(prompts, stable_cog, ctx, message)
+        await message.edit(view=view)
 
 
 def setup(bot):
