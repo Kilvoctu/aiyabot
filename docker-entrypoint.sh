429bdc2ab560434d9a98d02765977e8261ff7752
FoxxMD <FoxxMD@users.noreply.github.com>
2024-02-27 14:02:07 -0500

    Cleanup docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
+USE_GEN="${USE_GENERATE:=true}"
+
 # requirements are installed at runtime instead of during image build
 # so that image isn't 6GB+ due to nvidia/torch package sizes
 #
@@ -11,15 +13,12 @@ else
   printf '****************\nInstalling requirements, this may take some time!\n****************\n'
 fi
 
-USE_GEN="${USE_GENERATE:=true}"
-
 if [ "$USE_GEN" = "true" ]; then
   pip install -r requirements.txt
 else
+  # don't use full requirements if user explicitly disabled /generate command
   pip install -r requirements_no_generate.txt
 fi
-
-#pip install -r requirements.txt
 
 printf '****************\nRequirements satisfied!\n****************\n'
 
@@ -27,9 +26,11 @@ printf '****************\nRequirements satisfied!\n****************\n'
 cp -n "/default/resources/messages.csv" "/app/resources/messages.csv"
 cp -n "/default/outputs/.keep" "/app/outputs/.keep"
 
-printf '****************\nChecking for required /generate files/models\n****************\n'
-# Download generate pre-reqs
-python core/setup_generate.py
+if [ "$USE_GEN" = "true" ]; then
+  printf '****************\nChecking for required /generate files/models\n****************\n'
+  # Download generate pre-reqs
+  python core/setup_generate.py
+fi
 
 printf '****************\nStarting Aiyabot\n****************\n'
 # Start the app.
